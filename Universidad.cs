71d3fc0034b976a8cc865beab080c135b61771ee
Francisco Pilarte <fpilarteespinosa@gmail.com>
2024-10-06 15:33:12 -0600

    se creo un clase Exrensiones con un metodo estatico que se utiliza para imprimir la informacion de los estudiante

--- Universidad.cs
+++ Universidad.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static tarea4.Universidad.Estudiante;
+using static tarea4.Universidad;
 
 namespace tarea4
 {
@@ -29,6 +31,27 @@ namespace tarea4
 
         }
 
+    }
+
+    public static class Extensiones
+    {
+        public static void MostrarInformacion(this Estudiante texto)
+        {
+            double asistencia = Asistencia.Porcentaje(texto.SesionesA, texto.SesionesT);
+            Console.WriteLine($"Nombre del Estudiante: {texto.Nombre}");
+            Console.WriteLine($"Total de Sesiones: {texto.SesionesT}");
+            Console.WriteLine($"Sesiones Asistidas: {texto.SesionesA}");
+            Console.WriteLine($"Porcentaje de Asistencia: {asistencia}%");
+
+            if (asistencia >= 75)
+            {
+                Console.WriteLine("El estudiante cumple con el requisito de asistencia.");
+            }
+            else
+            {
+                Console.WriteLine("El estudiante NO cumple con el requisito de asistencia.");
+            }
+        }
     }
 
 }
