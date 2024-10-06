39497fdad604768b4bb05e121b8211369b21ed67
Francisco Pilarte <fpilarteespinosa@gmail.com>
2024-10-06 14:59:53 -0600

    arreglo y lector

--- Program.cs
+++ Program.cs
@@ -6,8 +6,30 @@ namespace tarea4
     {
         public static void Main(string[] args)
         {
-            
 
+            int CaTotal = 0;
+
+            Console.WriteLine("Ingrese el Nombre del Grupo");
+            string seccion = Console.ReadLine();
+
+            do
+            {
+                Console.WriteLine("Ingrese la Asisitencia Total: ");
+                CaTotal = Convert.ToInt32(Console.ReadLine());
+                if (CaTotal < 0)
+                {
+                    Console.WriteLine("Ingrese un numero Positivo");
+                }
+
+            } while (CaTotal < 0);
+
+
+            Console.WriteLine("Ingrese la Cantidad de Estudiante");
+            int estudiante = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+            string[] CaNombre = new string[estudiante];
+            double[] CaAsis = new double[estudiante];
 
 
 
